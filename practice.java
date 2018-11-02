f7102f61be0dfc99bccba534c8e754c9d4529675
kimjooeun <32117207+kimjooeun@users.noreply.github.com>
2018-11-02 14:49:42 +0900

    Update practice.java

--- practice.java
+++ practice.java
@@ -1,31 +1,28 @@
+/*백준알고리즘 9095번*/
+
 package practice;
 
-import java.util.Arrays;
+import java.util.Scanner;
+public class practice {
 
-public class practice implements Comparable<practice>{
-	public String name;
-	public int quantity;
-	public practice(String name, int quantity) {
-		this.name = name;
-		this.quantity = quantity;
-	}
-	
-	public int compareTo(practice other) {
-		return quantity - other.quantity;
-	}
-
-	
 	public static void main(String[] args) {
-		practice[] fruits = new practice[3];
-		fruits[0] = new practice("pineapple", 70);
-		fruits[1] = new practice("apple", 100);
-		fruits[2] = new practice("orange", 80);
-		
-		Arrays.sort(fruits);
-		
-		for(practice name: fruits) {
-			System.out.println(name);
+		// TODO Auto-generated method stub
+		 int[] arr = new int[11];
+		    int n;
+		    Scanner scanner = new Scanner(System.in);
+		     
+		    arr[0] = 0; // 정수가 0일 때 방법(경우)의 수
+		    arr[1] = 1; // 정수가 1일 때 경우의 수, 1 => 1개
+		    arr[2] = 2; // 정수가 2일 때 경우의 수, 1+1, 2 => 2개
+		    arr[3] = 4; // 정수가 3일 때 경우의 수, 1+1+1, 1+2, 2+1, 3 => 4개
+		    
+		    System.out.print("n을 입력하세요: ");
+		    n = scanner.nextInt();
+		      for(int j = 4; j <= n; j++){
+		        arr[j] = arr[j-1] + arr[j-2] + arr[j-3];
+		      }
+		    System.out.println(arr[n]);
+		    scanner.close();
 		}
-		
-	}
 }
+
